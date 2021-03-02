28998433de7589372f54bb353bbec51c76977c83
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-03-02 14:22:24 +0530

    Update anime.py

--- SaitamaRobot/modules/anime.py
+++ SaitamaRobot/modules/anime.py
@@ -590,7 +590,7 @@ dispatcher.add_handler(KAIZOKU_SEARCH_HANDLER)
 dispatcher.add_handler(KAYO_SEARCH_HANDLER)
 dispatcher.add_handler(UPCOMING_HANDLER)
 
-__mod_name__ = "Anime"
+__mod_name__ = "Anime👸"
 __command_list__ = [
     "anime", "manga", "character", "user", "upcoming", "kaizoku", "airing",
     "kayo"
